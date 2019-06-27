41ab98842b45a073485b6a009bcc4d7247d32c7b
MD. Rakibul Hossain <rakibul.hossain365@gmail.com>
2019-06-28 01:31:43 +0600

    hotel search added with postgres searcevector

--- hotel/views.py
+++ hotel/views.py
@@ -1,16 +1,35 @@
 import pandas as pd
 
 from django.shortcuts import render
+from django.contrib.postgres.search import SearchVector
 
 from .models import HotelDetail
 from ml_engine.models import RecommendedData
 
 
 def index(request):
-    recommended_ids = RecommendedData.objects.get(id=1).hotel_id
-    id_list = [int(val) for val in recommended_ids.split(",")]
-    hotel_details = HotelDetail.objects.filter(hotel_id__in=id_list)
+    if request.POST:
+        post_data = request.POST
+        place = post_data['place']
+        checkin_date = post_data['check_in_date']
+        checkout_date = post_data['check_out_date']
+        guest = post_data['guests']
 
-    return render(request, 'hotel_app/index.html', {
-        'hotel_details': hotel_details,
-    })
+        hotels_data = HotelDetail.objects.annotate(
+            search=SearchVector('district', 'country', 'address'),
+        ).filter(search=place)
+
+        # results = BlogPost.objects.annotate(
+        #     search=SearchVector('title', 'intro', 'content'),
+        # ).filter(search=your_search_query)
+
+        print(place)
+        print(checkin_date)
+        print(checkout_date)
+        print(guest)
+
+        return render(request, "hotel_app/search_result.html", {
+            'hotels_data': hotels_data
+        })
+
+    return render(request, "hotel_app/index.html")
