f00c620a35ba8eb6149b9238b95c2e8a8a155f63
MD. Rakibul Hossain <rakibul.hossain365@gmail.com>
2019-07-10 03:02:45 +0600

    default database syntax solved

--- proj/settings.py
+++ proj/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'proj.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES[‘default’].update(db_from_env)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
