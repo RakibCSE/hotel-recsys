04e586ec34ae43817247463960f731e98d821fc7
MD. Rakibul Hossain <rakibul.hossain365@gmail.com>
2019-07-10 03:06:31 +0600

    dj database url deleted

--- proj/settings.py
+++ proj/settings.py
@@ -3,7 +3,6 @@ import json
 import os
 
 import django_heroku
-import dj_database_url
 
 from celery.schedules import crontab
 
@@ -83,9 +82,12 @@ WSGI_APPLICATION = 'proj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
